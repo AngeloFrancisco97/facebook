88d66a36763791d333ba3c7dd73a0975a03948bc
Angelo Francisco <angelofrancisco@outlook.com>
2020-11-18 18:12:10 -0300

    Primeiro commit facebook

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,143 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.1.1">
+    <title>Starter Template · Bootstrap</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/fontawesome.min.css')}}">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+  </head>
+  <body style="background: #F0F2F5 ">
+      <nav class="mt-0 pt-1 pb-0 navbar navbar-expand-md navbar-light" style="background: white;">
+  <a class="navbar-brand" href="#"><i class="fab fa-facebook facebook"></i></a>
+  <form class="form-inline my-2 my-lg-0">
+    <input class="form-control pesquisa" type="text" placeholder="Search" aria-label="Search">
+  </form>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item active">
+          <div class="icone-ativo">
+            <a class="nav-link" href="#"><i class="fas fa-home icones icone-azul"></i><span class="sr-only">(current)</span></a>
+          </div>
+      </li>
+      <li class="nav-item">
+          <div class="">
+            <a class="nav-link" href="#"><i class="fas fa-tv icones "></i><span class="sr-only">(current)</span></a>
+          </div>
+      </li>
+      <li class="nav-item">
+          <div class="">
+            <a class="nav-link" href="#"><i class="fas fa-store icones "></i><span class="sr-only">(current)</span></a>
+          </div>
+      </li>
+      <li class="nav-item">
+          <div class="">
+            <a class="nav-link" href="#"><i class="fas fa-users icones"></i><span class="sr-only">(current)</span></a>
+          </div>
+      </li>
+      <li class="nav-item">
+          <div class="">
+            <a class="nav-link" href="#"><i class="fas fa-gamepad icones"></i><span class="sr-only">(current)</span></a>
+          </div>
+      </li>
+    </ul>
+    <div class="form-inline my-2 my-lg-0">
+        <div class="mr-2">Angelo</div>
+        <div class="mr-2 circulo_icones"><i class="fas fa-plus"></i></div>
+        <div class="mr-2 circulo_icones"><i class="fab fa-facebook-messenger"></i></div>
+        <div class="mr-2 circulo_icones"><i class="fas fa-bell"></i></div>
+        <div class="mr-2 circulo_icones"><i class="fas fa-caret-down" style="font-size: 22px"></i></div>
+    </div>
+  </div>
+</nav>
+
+<main role="main" style="background-color: #F0F2F5">
+    <div class="menu-lateral-esqeurdo">
+        <div><i class="pt-2 fas fa-user"></i>Angelo Francisco</div>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">COVID-19: Central de Informações</div></div>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">Amigos</div></div>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">Grupos</div></div>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">Marketplace</div></div>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">Videos</div></div>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">Eventos</div></div>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">Lembranças</div></div>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">Salvos</div></div>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">Ver mais</div></div>
+        <hr>
+        <h5>Seus Atalhos</h5>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">Coin Master</div></div>
+        <div class="d-flex pt-3"><i class="fas fa-bell"></i><div class="ml-2">Outras coisas</div></div>
+    </div>
+    <div class="container container-central">
+        <div class="d-flex">
+            <div class="story">
+                
+            </div>
+            <div class="story">
+                
+            </div>
+            <div class="story">
+                
+            </div>
+            <div class="story">
+                
+            </div>
+            <div class="story">
+                
+            </div>
+        </div>
+        <div class="d-flex mt-4 justify-content-center" style="width: 100%;" >
+            <div class="justify-content-center d-flex flex-column p-3 align-items-center" style="background-color: white; border-radius: 20px; width: 90%; height: 100%;">
+                <div class="w-100 d-flex align-items-center">
+                    <div class="">Angelo</div>
+                    <form class="w-100">
+                        <input class="ml-2 form-control w-100" style="border-radius: 30px; background: #F0F2F5" type="text" name="name" placeholder="O que você está pensando.">
+                    </form> 
+                </div> 
+                <hr class="w-100">
+                <div class="w-100 d-flex justify-content-center align-items-center">
+                    <div class=""><i class="fas fa-video mr-2" style="color: red"></i>Videos ao vivo</div> 
+                    <div class=""><i class="fas fa-camera-retro mr-2 ml-3" style="color: green"></i>Foto/vídeo</div> 
+                    <div class=""><i class="fas fa-grin-beam mx-2 ml-3" style="color: orange"></i>Semtimento/atividade</div> 
+                </div> 
+            </div>  
+            
+        </div>
+        
+    </div>
+
+</main><!-- /.container -->
+	<script src="{{ asset('js/jquery.js')}}" type="text/javascript"></script>
+        <script src="https://kit.fontawesome.com/b11c4b137e.js" crossorigin="anonymous"></script>
+</html>
